51b8825c9d52b9d2e253e53bfb772b02d3ada307
RainerKl <rainer@kleindesign.ee>
2021-09-17 16:38:22 +0200

    Exercise to test the law of large numbers, where increasing n leads to the average of the sample nearing the statistical expected mean.

---
+++ lln.py
@@ -0,0 +1,19 @@
+import numpy
+from numpy.lib.nanfunctions import nanargmax
+from numpy.random import randn
+
+while True:
+    try:
+        n = int(input("Specify sample size: "))
+        break
+    except ValueError:
+        print("Please enter an integer")
+
+counter=0
+for idx, val in enumerate(randn(n)):
+    if abs(val)<1:
+        counter+=1
+        
+ratio=round(counter/n*100,4)
+
+print(f'Out of {n} samples, the values was within +-1 in {ratio}%.')
